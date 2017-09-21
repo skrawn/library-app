9ee9c78867e569ef4ae4597d37e61520b748c5d4
Sean Donohue <skrawn@skrawn.mcnetface>
2017-09-20 22:04:31 -0600

    Removed the contact controller and moved most of that functionality into the contact model. Still need to get the responseMessage to work.

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,6 +1,12 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  message: DS.attr('string')
+  message: DS.attr('string'),
+
+  emailValid: Ember.computed.match('email', /^.+@.+\..+$/),
+  hasMessage: Ember.computed.gte('message.length', 5),
+  isValid: Ember.computed.and('emailValid', 'hasMessage'),
+  inNotValid: Ember.computed.not('isValid')
 });
--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+        return this.store.createRecord('contact');
+    },
+
+    actions: {
+        saveContact(newContact) {
+            newContact.save().then(() => {
+                newContact.set('email', '');
+                newContact.set('message', '');
+            });
+        },
+
+        willTransition() {
+            this.controller.get('model').rollbackAttributes();
+        }
+    }
 });
